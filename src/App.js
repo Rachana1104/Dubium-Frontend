a54e113e10f104f31409c8faa6e028cc142dbade
Om Shukla <shuklaom1208@gmail.com>
2021-12-11 14:40:27 +0530

    Integrated with Hosted APIs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
 import UserName from "./pages/Auth/UserName";
 import Confirmation from "./pages/Auth/Confirmation";
+import Verification from "./pages/Auth/Verify";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 var userDetailContext = React.createContext();
@@ -15,19 +16,12 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route path="/SignUp">
-            <SignUp />
-          </Route>
-          <Route path="/Username">
-            <UserName />
-          </Route>
+          <Route exact path="/" component={Login} />
+          <Route path="/SignUp" component={SignUp} />
+          <Route path="/Username" component={UserName} />
+          <Route path="/verify/token=:tok" component={Verification} />
           <userDetailContext.Provider value={userName}>
-            <Route path="/Confirmation">
-              <Confirmation />
-            </Route>
+            <Route path="/Confirmation" component={Confirmation} />
           </userDetailContext.Provider>
         </Switch>
       </Router>
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -10,6 +10,7 @@ import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import { Grid } from "@mui/material";
 import { Link } from "react-router-dom";
+import { GoogleLogin } from 'react-google-login'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Login = () => {
   const classes = useStyles();
-  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
@@ -49,6 +50,54 @@ const Login = () => {
     event.preventDefault();
   };
 
+  async function authConfirm(token) {
+    var item = { "auth_token": token }
+
+    let result = await fetch('https://dubium-apis.herokuapp.com/account/google/', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': '*/*'
+      }
+    })
+    result = await result.json()
+    console.log(result)
+  }
+
+  function responseGoogle(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var auth_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + auth_token);
+
+    authConfirm(auth_token);
+  }
+
+  async function log_in() {
+
+    let item = { email, password }
+
+    let result = await fetch('https://dubium-apis.herokuapp.com/account/login/', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': '*/*'
+      }
+    })
+    result = await result.json()
+    console.log(result)
+  }
+
   return (
     <div className={classes.root}>
       <Grid container spacing={0}>
@@ -58,60 +107,68 @@ const Login = () => {
         <div className="content">
           <center><h2>Welcome back User</h2></center>
           <Grid item md={6} sm={12}>
-            <form>
-              <TextField
-                label="Username"
-                variant="filled"
-                required
-                value={userName.trim()}
-                onChange={(e) => setUserName(e.target.value)}
-                className="login_username"
-                style={{ borderRadius: 4 }}
-              />
-              <br />
-              <TextField
-                label="Password"
-                variant="filled"
-                type="password"
-                required
-                value={password.trim()}
-                onChange={(e) => setPassword(e.target.value)}
-                style={{ borderRadius: 4 }}
-                // endAdornment={
-                //   <InputAdornment position="end">
-                //     <IconButton
-                //       aria-label="toggle password visibility"
-                //       onClick={handleClickShowPassword}
-                //       onMouseDown={handleMouseDownPassword}
-                //       edge="end"
-                //     >
-                //       {showPassword ? <VisibilityOff /> : <Visibility />}
-                //     </IconButton>
-                //   </InputAdornment>
-                // }
-              />
-              <Link href="#">
-                <h6>Forgot your Password?</h6>
-              </Link>
-              <Button
-                type="submit"
-                variant="contained"
-                className="login"
-                style={{ width: 300 }}
-              >
-                Login
-              </Button>
-            </form>
-            <div className="or">OR</div>
+            <TextField
+              label="Email"
+              variant="filled"
+              required
+              value={email.trim()}
+              onChange={(e) => setEmail(e.target.value)}
+              className="login_username"
+              style={{ borderRadius: 4 }}
+            />
+            <br />
+            <TextField
+              label="Password"
+              variant="filled"
+              type="password"
+              required
+              value={password.trim()}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{ borderRadius: 4 }}
+            // endAdornment={
+            //   <InputAdornment position="end">
+            //     <IconButton
+            //       aria-label="toggle password visibility"
+            //       onClick={handleClickShowPassword}
+            //       onMouseDown={handleMouseDownPassword}
+            //       edge="end"
+            //     >
+            //       {showPassword ? <VisibilityOff /> : <Visibility />}
+            //     </IconButton>
+            //   </InputAdornment>
+            // }
+            />
+            <Link href="#">
+              <h6>Forgot your Password?</h6>
+            </Link>
             <Button
               type="submit"
-              variant="outlined"
-              color="secondary"
-              style={{ paddingLeft: 10, width: 300 }}
-              className="google"
+              variant="contained"
+              className="login"
+              style={{ width: 300 }}
+              onClick={log_in}
             >
-              Google
+              Login
             </Button>
+            <div className="or">OR</div>
+            <GoogleLogin
+              clientId="46358633355-dokobqrl0fu620jvj5itidc3vr6afl9a.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+              theme='dark'
+              // icon={false}
+              render={renderProps => (
+                <Button
+                  onClick={renderProps.onClick}
+                  type="submit"
+                  variant="outlined"
+                  color="secondary"
+                  style={{ width: 310 }}
+                  className="create" > Login with Google </Button>
+              )}
+            />
             <br />
             <Link to="/SignUp">
               <Button
